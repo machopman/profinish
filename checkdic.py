acbeb607c0d81599c5f83bcb9785dff42c4f2e55
macho <machopman_shop@hotmail.com>
2018-05-17 00:46:42 +0700

    ojkjikj

--- checkdic.py
+++ checkdic.py
@@ -5,34 +5,43 @@ from flask import json
 
 from cutword import cutw
 import re
-def searchMovieNameInDic(question):
-    cut = cutw(question)
-    ques =question
-    t = []
-    with open('new.txt', mode='r', encoding='utf-8-sig') as f:
-        a = load(f)
 
-        for key, value in a.items():
+from searchMovieNameInDic import searchMovieNameInDic
 
-            for i in cut:
-                if i in value:
-                    w = ques.replace(i,'')
-                    t.append(w)
-                else:
-                    z = difflib.get_close_matches(i, value)
-                    if z != []:
-                        u = ques.replace(i, '')
-                        t.append(u)
-    if t ==[]:
-        return ''
+
+def searchMovieName(question):
+    sentence = re.sub('[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]', '', question).replace(' ', '')
+    name =  searchMovieNameInDic(question)
+    if sentence !='' and name !='':
+        cut = cutw(question)
+        ques =question
+        t = []
+        with open('new.txt', mode='r', encoding='utf-8-sig') as f:
+            a = load(f)
+
+            for key, value in a.items():
+
+                for i in cut:
+                    if i in value:
+                        w = ques.replace(i,'')
+                        t.append(w)
+                    else:
+                        z = difflib.get_close_matches(i, value)
+                        if z != []:
+                            u = ques.replace(i, '')
+                            t.append(u)
+        if t ==[]:
+            return ''
+        else:
+            return t[0]
     else:
-        return t[0]
+        return ''
 
 
 
 
 def checkd(question):
-    name = searchMovieNameInDic(question)
+    name = searchMovieName(question)
     sentence = re.sub('[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]', '', question).replace(' ', '')
     if name !='':
         return name
@@ -40,5 +49,5 @@ def checkd(question):
         return sentence
     elif sentence =='' and name =='':
         return question
-#print(checkd('ขอรีวิวwonderwoman'))
+print(checkd('ขอรีวิวwonderwoman'))
 
