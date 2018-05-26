d33b1371a1abb860b1cfa98c30f30262e3b8962d
macho <machopman_shop@hotmail.com>
2018-05-26 20:07:57 +0700

    esrfewrfwrf

--- actor.py
+++ actor.py
@@ -27,7 +27,6 @@ def movie_actor(event,findm,question):
                 movie_detail = r.json()
                 detail = movie_detail['response'][0]['detailMovie'][0]['Actor']
                 detail = detail.replace('\n', '')
-                print(detail)
                 if detail != '':
                     return detail
                 else:
@@ -35,31 +34,30 @@ def movie_actor(event,findm,question):
         if found == False:
             return 'ยังไม่มีข้อมูลนักแสดงหนังเรื่องนี้เลยครับ'
     elif (movie_name == '') and (searchMovieNameInDic(question) == ''):
-        mov = findm
-        movie_name = mov.lower().replace(' ','')
-        URL = "http://mandm.plearnjai.com/API/id_nameMovie.php?key=mandm"
-        r = requests.get(url=URL)
-        data = r.json()
-        found = False
-        for movie in data:
-            if movie_name == movie['nameEN'].lower().replace(' ', ''):
-                print(movie_name)
-                found = True
-                Movie_URL = 'http://mandm.plearnjai.com/API/detailMovie.php?idmovie=' + movie['idIMDb']
-                r = requests.get(url=Movie_URL)
-                movie_detail = r.json()
-                detail = movie_detail['response'][0]['detailMovie'][0]['Actor']
-                detail = detail.replace('\n', '')
-                print(detail)
-                if detail != '':
-                    return detail
-                else:
-                    return 'ยังไม่มีข้อมูลนักแสดงหนังเรื่องนี้เลยครับ'
-        if found == False:
-            return 'ยังไม่มีข้อมูลนักแสดงหนังเรื่องนี้เลยครับ'
+            mov = findm
+            movie_name = mov.lower().replace(' ', '')
+            URL = "http://mandm.plearnjai.com/API/id_nameMovie.php?key=mandm"
+            r = requests.get(url=URL)
+            data = r.json()
+            found = False
+            for movie in data:
+                if movie_name == movie['nameEN'].lower().replace(' ', ''):
+                    found = True
+                    Movie_URL = 'http://mandm.plearnjai.com/API/detailMovie.php?idmovie=' + movie['idIMDb']
+                    r = requests.get(url=Movie_URL)
+                    movie_detail = r.json()
+                    detail = movie_detail['response'][0]['detailMovie'][0]['Actor']
+                    detail = detail.replace('\n', '')
+                    print(detail)
+                    if detail != '':
+                        return detail
+                    else:
+                        return 'ยังไม่มีข้อมูลนักแสดงหนังเรื่องนี้เลยครับ'
+            if found == False:
+                return 'ยังไม่มีข้อมูลนักแสดงหนังเรื่องนี้เลยครับ'
 
 
-        else:
+    else:
             cut = cutw(event.message.text)
             with open('new.txt', mode='r', encoding='utf-8-sig') as f:
                 a = load(f)
